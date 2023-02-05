c757b7b7b8e05cf091e41d54bbd26294486dd1da
INDRESH BHARATI <85506515+theindresh@users.noreply.github.com>
2023-02-06 03:03:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('#menu').click(function(){
+        $(this).toggleClass('fa-times');
+        $('header').toggleClass('toggle');
+    });
+    $(window).on('scroll load', function(){
+        $('#menu').toggleClass('fa-times');
+        $('header').toggleClass('toggle'); 
+    });
+
+    });
